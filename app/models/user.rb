fc9c80eba35e29662dccc48e4aa9086bfe43a2ee
n8 <teamtomkins23@gmail.com>
2014-10-25 18:24:15 -0500

    temporarily removed devise,  ready for basic layout framing

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+#  resources :users
 
   root 'static_pages#home'
 
   match '/dashboard',   to: 'static_pages#dashboard',     via: 'get'
   match '/help',        to: 'static_pages#help',          via: 'get'
   match '/calculators', to: 'static_pages#calculators',   via: 'get'
-
-  devise_for :users  
+  
 end
