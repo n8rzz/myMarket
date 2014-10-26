7ea1a7f864395254c434292f958320101913ffd9
n8 <teamtomkins23@gmail.com>
2014-10-25 23:52:07 -0500

    added layout link tests, split main layout into pieces

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   match '/help',        to: 'static_pages#help',          via: 'get'
   match '/calculators', to: 'static_pages#calculators',   via: 'get'
   match '/top10',		to: 'static_pages#top10',		  via: 'get'
+  match '/about',		to: 'static_pages#about',		  via: 'get'
 end
