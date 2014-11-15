dfd1fceccd703d1acfad972cb0139d13af5721e7
n8 <teamtomkins23@gmail.com>
2014-11-14 22:46:31 -0600

    split dashboard in to partials, filled in portfolio and transactions tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-# match '/dashboard',   to: 'static_pages#dashboard',     via: 'get'
+
   match '/help',        to: 'static_pages#help',          via: 'get'
   match '/calculators', to: 'static_pages#calculators',   via: 'get'
   match '/about',		    to: 'static_pages#about',    		  via: 'get'
@@ -8,10 +8,11 @@ Rails.application.routes.draw do
   match '/login',       to: 'sessions#new',               via: 'get'
   match '/login',       to: 'sessions#create',            via: 'post'
   match '/logout',      to: 'sessions#destroy',           via: 'delete'
+  match '/dashboard',   to: 'users#show',                 via: 'get'
 
   resources :users
   resources :account_activations, only: [:edit]
-  
+
   get 'sessions/new'
   get 'users/new'
 end
