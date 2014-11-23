8db4914955ec30ccb5772be2c6945ebb9955a2fa
n8 <teamtomkins23@gmail.com>
2014-11-22 23:58:11 -0600

    glyphicons for user and session new forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: [:index, :destroy]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-#  	debugger
   end
 
   def new
