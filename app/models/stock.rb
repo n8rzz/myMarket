2a9675be3a78baec93927ddbc5fc47e7fa32b2f2
n8 <teamtomkins23@gmail.com>
2014-11-29 00:17:07 -0600

    continued #11, specific-test.  stocks controller tests basic CRUD functions

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,7 +4,10 @@ class Stock < ActiveRecord::Base
   before_save { self.ticker = ticker.upcase }
 
   default_scope -> { order('created_at ASC') }
-  validates :user_id, :shares, :entryprice, :entryfee, :entrydate, presence: true
+  validates :user_id, presence: true
+  validates :shares, presence: true, numericality: {greater_than_or_equal_to: 1.0}
+  validates :entryprice, :entryfee, presence: true, numericality: {greater_than_or_equal_to: 0.0001}
+  validates :entrydate, presence: true
   validates :ticker, presence: true, length: { maximum: 6 }
 
   def current_price
