710564eee50e7fbcc88f6d0e915a168ed92852dd
n8 <teamtomkins23@gmail.com>
2014-11-28 19:34:38 -0600

    completed #19. entry/exit dates for option model, controller, new/edit/index and user/index views

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,13 +1,7 @@
 class Option < ActiveRecord::Base
   belongs_to :user
-  validates :user_id, presence: true
 
+  validates :user_id, :side, :optiontype, :contracts, :size, :entryprice, :entryfee, :entrydate, presence: true
   validates :ticker, presence: true, length: { maximum: 6 }
-  validates :side, presence: true
-  validates :optiontype, presence: true
-  validates :contracts, presence: true
-  validates :size, presence: true
-  validates :entryprice, presence: true
-  validates :entryfee, presence: true
 
 end
